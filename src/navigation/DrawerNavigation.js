cb9c792206020b5f48145b27dbb00ec6f60fe119
Darko2102 <snezana.danicic.77@gmail.com>
2025-11-13 21:05:14 +0100

    module19

--- src/navigation/DrawerNavigation.js
+++ src/navigation/DrawerNavigation.js
@@ -11,21 +11,47 @@ const DrawerNavigator = () => {
             drawerActiveBackgroundColor: '#2e4d92ff',
             drawerActiveTintColor: 'white',
             drawerInactiveTintColor: 'black',
-        }}>
+            drawerStyele: {
+                width: 250,
+            },
+        }}> 
+        
+        
+             
             <Drawer.Screen name="Home" component={BottomTabNavigator}
+
                 options={{
                     title: "Home",
                     drawerIcon: ({focused, color, size}) => (
                         <MaterialCommunityIcons name={focused ? "home": "home-outline"} size={22} color={color}/>
                     )
-                }}
-            />
+                }}/>  
+          
+        
             <Drawer.Screen name="About" component={AboutStackNavigator}
                 options={{
                     title: "Home",
                     drawerIcon: ({focused, color, size}) => (
                         <MaterialCommunityIcons name={focused ? "information": "information-outline"} size={22} color={color}/>
                     )
+                }}
+            />
+        
+            <Drawer.Screen name="Contact" component={ContactStackNavigator}
+                options={{
+                    title: "Contact",
+                    drawerIcon: ({focused, color, size}) => (
+                        <MaterialCommunityIcons name={focused ? "contact": "contact-outline"} size={22} color={color}/>
+                 )
+                }}
+            />
+        
+            <Drawer.Screen name="Profile" component={ProfileStackNavigator}
+                options={{
+                    title: "Profile",
+                    drawerIcon: ({focused, color, size}) => (
+                        <MaterialCommunityIcons name={focused ? "profile": "profile-outline"} size={22} color={color}/>
+                 )
                 }}
             />
         </Drawer.Navigator>
