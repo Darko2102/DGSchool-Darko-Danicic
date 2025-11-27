5b1c648417242ea73c97c6596a9c5100cd39e31f
Darko2102 <snezana.danicic.77@gmail.com>
2025-11-27 21:00:04 +0100

    module22

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,98 +1,161 @@
-import React from 'react';
-import { StyleSheet, View, Text, Button, Image} from 'react-native';
-import Swiper from 'react-native-swiper';
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  FlatList,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import Swiper from "react-native-swiper";
+import Icon from "../components/Icon";
+import data from "../../data/products.json";
+import Item from "../components/Item";
 
-const Home = ({navigation}) => {
-    return(
-        <View style={StyleSheet.container}>
-           <View style={styles.sliderContainer}>
-                <Swiper
-                    style={styles.item}
-                    autoplay={true}
-                    activeDotColor='#22D4FF'
-                    autoplayTimeout={3}
-                    activeDot="#11101022"
-                >
-                    <View>
-                        {/* <Image style={styles.imgItem}  source={require('../../assets/OIP.jpg')} resizeMode="cover"/>
-                        <Image style={styles.imgItem}  source={require('../../assets/assets/sample-7705346_1280.jpg')} resizeMode="cover"/>
-                        <Image style={styles.imgItem}  source={require('../../assets/assets/sample-7705350_1280.jpg')} resizeMode="cover"/>
-                        <Image style={styles.imgItem}  source={require('../../assets/assets/assets/Nikon-Z8-Official-Samples-00002.jpg')} resizeMode="cover"/>
-                        <Image style={styles.imgItem}  source={require('../../assets/assets/assets/pexels-agrosales-20804701.jpg')} resizeMode="cover"/> */}
-                        <Text style={styles.txtColor1}>1</Text>
-                        <Text style={styles.txtColor1}>2</Text>
-                        <Text style={styles.txtColor1}>3</Text>
-                    </View>
-                    <View>
-                        <Text style={styles.txtColor2}>1</Text>
-                        <Text style={styles.txtColor2}>2</Text>
-                        <Text style={styles.txtColor2}>3</Text>
-                    </View>
-                    <View>
-                        <Text style={styles.txtColor3}>1</Text>
-                        <Text style={styles.txtColor3}>2</Text>
-                        <Text style={styles.txtColor3}>3</Text>
-                    </View>
-                </Swiper>
-                
-           </View>
-                <View style={styles.iconsContainer}>
-                    <Icon name="cellphone" iconText="Iphone" size={22} color={"#398858ff"}></Icon>
-                    <Icon name="android" iconText="Samsung" size={21} color={"#2b746aff"}></Icon>
-                    <Icon name="laptop" iconText="Laptop" size={23} color={"#504b92ff"}></Icon>
+class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      products: data,
+    });
+  }
+
+  render() {
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={styles.sliderContainer}>
+            <Swiper
+              autoplay={true}
+              activeDotColor="rgba(34, 212, 255, 1)"
+              autoplayTimeout={5}
+            >
+              <View style={styles.item}>
+                <Image
+                  style={styles.imgItem}
+                  source={require("../../assets/banner_1.jpg")}
+                  resizeMode="cover"
+                />
+              </View>
+              <View style={styles.item}>
+                <Image
+                  style={styles.imgItem}
+                  source={require("../../assets/banner_2.png")}
+                  resizeMode="cover"
+                />
+              </View>
+              <View style={styles.item}>
+                <Image
+                  style={styles.imgItem}
+                  source={require("../../assets/banner_3.jpeg")}
+                  resizeMode="cover"
+                />
+              </View>
+              <View style={styles.item}>
+                <Image
+                  style={styles.imgItem}
+                  source={require("../../assets/banner_5.jpg")}
+                  resizeMode="cover"
+                />
+              </View>
+            </Swiper>
+          </View>
+          <View style={styles.iconsContainer}>
+            <Icon name="cellphone" iconText="Iphone"></Icon>
+            <Icon name="android" iconText="Samsung"></Icon>
+            <Icon name="laptop" iconText="Laptop"></Icon>
+          </View>
+          <View style={styles.iconsContainer}>
+            <Icon name="tablet" iconText="Tablet"></Icon>
+            <Icon name="mouse" iconText="Mouse"></Icon>
+            <Icon name="keyboard-outline" iconText="Keyboard"></Icon>
+          </View>
+
+          <View style={styles.productsContainer}>
+            <Text style={styles.title}>Most Popular Products</Text>
+            <FlatList
+              data={this.state.products.popularproducts}
+              renderItem={({ item }) => (
+                <View>
+                  <Item item={item} />
                 </View>
-                <View style={styles.iconsContainer}>
-                    <Icon name="tablet" iconText="Tablet" size={25} color={"#b6ab47ff"}></Icon>
-                    <Icon name="mouse" iconText="Mouse" size={24} color={"#ad4545ff"}></Icon>
-                    <Icon name="keyboard-outline" iconText="Keyboard" size={23} color={"#ac3fb6ff"}></Icon>
-                </View>
+              )}
+            />
+            <TouchableOpacity style={styles.btn}>
+              <Text style={styles.btnText}>View More</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-    )
+      </ScrollView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    iconsContainer:{
-        width: '90%',
-        alignSelf: "center",
-        marginTop:30,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-        width: '100%',
-    },
+  iconsContainer: {
+    width: "90%",
+    alignSelf: "center",
+    marginTop: 30,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
 
-    sliderContainer:{
-        
-        height: 200,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 10,
-        borderRadius: 8,
-        },
-        item:{
-            flex: 1,
-            justifyContent: 'center',
-        },
-        imgItem: {
-            width: '100%',
-            height: '100%',
-            borderRadius: 8,
-        },
-        txtColor1: {
-            backgroundColor: '#5b509cff'
-        },
-        txtColor2: {
-            backgroundColor: '#3c6e8bff'
-        },
-        txtColor3: {
-            backgroundColor: '#25776cff'
-        },
-        
-})
+  container: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "#fff",
+  },
+
+  sliderContainer: {
+    height: 200,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
+    borderRadius: 8,
+  },
+
+  imgItem: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 8,
+  },
+
+  item: {
+    flex: 1,
+    justifyContent: "center",
+  },
+
+  productsContainer: {
+    marginHorizontal: 20,
+  },
+
+  title: {
+    marginTop: 35,
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+
+  btn: {
+    backgroundColor: "#22D4FF",
+    height: 50,
+    borderRadius: 8,
+    justifyContent: "center",
+    marginBottom: 20,
+  },
+
+  btnText: {
+    color: "white",
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+});
+
 export default Home;
